48aefd2e849656ff01e3dc63928439d8e22626bd
MiniatureEge2006 <hasanegesaral@gmail.com>
2025-02-28 18:02:34 +0300

    Added `cogs.roblox` with 2 roblox related commands. `g-ruserinfo` and `g-rgameinfo`. More may or may not come.

--- gman.py
+++ gman.py
@@ -37,7 +37,7 @@ async def set_prefix(guild_id, prefix):
         await conn.execute("INSERT INTO prefixes (guild_id, prefix) VALUES ($1, $2) ON CONFLICT (guild_id) DO UPDATE SET prefix = $2", guild_id, prefix)
 
 
-extensions = ['cogs.audio', 'cogs.help', 'cogs.ping', 'cogs.caption', 'cogs.code', 'cogs.exif', 'cogs.ffmpeg', 'cogs.tutorial', 'cogs.imagemagick', 'cogs.ytdlp', 'cogs.youtube', 'cogs.info', 'cogs.ai', 'cogs.reminder']
+extensions = ['cogs.audio', 'cogs.help', 'cogs.ping', 'cogs.caption', 'cogs.code', 'cogs.exif', 'cogs.ffmpeg', 'cogs.tutorial', 'cogs.imagemagick', 'cogs.ytdlp', 'cogs.youtube', 'cogs.info', 'cogs.ai', 'cogs.reminder', 'cogs.roblox']
 bot = commands.Bot(command_prefix=lambda bot, msg: get_prefix(msg), case_insensitive=True, strip_after_prefix=True, status=discord.Status.online, activity=discord.Game(name=f"{bot_info.data['prefix']}help"), help_command=None, intents=discord.Intents.all())
 
 
